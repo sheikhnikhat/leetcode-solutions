997d7a7423bd4aa1c670e38272b897b52a27047f
sheikhnikhat <sheikhnikhat62@gmail.com>
2026-08-27 10:16:49 +0530

    Time: 11 ms (96.25%), Space: 47.3 MB (96.15%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,31 +1,26 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-
-        for(int num : nums){
+        for (int num : nums){
             map.put(num, map.getOrDefault(num, 0) + 1);
-        }
+        } 
 
-        List<Integer>[] bucket = new List [nums.length + 1];
-
-        for(int key : map.keySet()){
-            int frequency = map.get(key);
-            if(bucket[frequency] == null){
-                bucket[frequency] = new ArrayList<>();
+        List <Integer> [] bucket = new List[nums.length + 1];
+        for (int key : map.keySet()){
+            int freq = map.get(key);
+            if (bucket[freq] == null){
+                bucket[freq] = new ArrayList<>();
             }
-            bucket[frequency].add(key);
+            bucket[freq].add(key);
         }
-        
-        int[] result = new int [k];
+        int[] result = new int[k];
         int counter = 0;
 
         for (int pos = bucket.length-1; pos >= 0 && counter < k; pos --){
-            if(bucket [pos] != null){
+            if (bucket [pos] != null){
                 for (int num : bucket[pos]){
-                    result[counter ++] = num;
-                    if (counter == k){
-                        return result;
-                    }
+                    result [counter++] = num;
+                    if (counter == k) return result;
                 }
             }
         }
